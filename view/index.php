7ad382c638906845406c47fea6e88d07014743ea
Anastasia Mishin <am988@afsconnect1.njit.edu>
2017-11-27 22:24:37 -0500

    view

---
+++ view/index.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+<main>
+    <h1>Menu</h1>
+    <ul>
+        <li>
+            <a href="product_manager">Product Manager</a>
+        </li>
+        <li>
+            <a href="product_catalog">Product Catalog</a>
+        </li>
+    </ul>
+</main>
+<?php include 'footer.php'; ?>
